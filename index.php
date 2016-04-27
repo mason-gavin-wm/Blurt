b1b9c1b0d75a1a25f6b9de97394fe571545f1ceb
Start Coding Student 1 <session1@start-coding-mbp-c02pp71ug8wn.local>
2016-04-27 14:03:27 -0700

    beginning stages of blurt

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include_once('authenticate.php');
+?>
+<!DOCTYPE html>
+    <html>
+<head>
+    <title>Blurt</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,5 @@
+<?php
+echo '<a href="index.php">Posts</a>&infin;';
+echo '<a href="view.php">My Posts</a>&infin;';
+echo '<a href="editprofile.php">Edit Profile</a>';
+?>
